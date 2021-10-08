38f8d2ca9b3c97e5c11c3cb7edc7159fb67083e5
Ereh Maryann <edwardmaryann09@gmail.com>
2021-10-08 13:46:56 -0700

    added the score text

--- script.js
+++ script.js
@@ -77,9 +77,9 @@ function renderCanvas() {
   context.fill();
 
   // Score
-  // context.font = '32px Courier New';
-  // context.fillText(playerScore, 20, canvas.height / 2 + 50);
-  // context.fillText(computerScore, 20, canvas.height / 2 - 30);
+  context.font = '32px Courier New';
+  context.fillText(playerScore, 20, canvas.height / 2 + 50);
+  context.fillText(computerScore, 20, canvas.height / 2 - 30);
 }
 
 // Create Canvas Element
